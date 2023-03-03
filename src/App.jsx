e9c4342ff547747464f9c5141fbb4a13acafaaf7
Harsh Verma <harsh.verma@botmantra.com>
2023-03-03 16:03:51 +0530

    add card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Card from "./Components/Card";
 import "./css/style.css"
 
 const App = () => {
-  return <div>This webpack 5 boiler plate</div>;
+  return <div >
+
+    <Card />
+
+  </div>;
 };
 
 export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className='card-container'>
+            Hello
+        </div>
+    )
+}
+
+export default Card
